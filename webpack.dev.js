2d32e428d0870ba8d3f7acdd910cb33d60c01101
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2025-01-18 05:54:41 +0000

    Setup app container and styling

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,18 @@
 const { merge } = require(`webpack-merge`);
 const common = require(`./webpack.common.js`);
+const { EnvironmentPlugin } = require("webpack");
+require("dotenv").config();
 
 module.exports = merge(common, {
-    mode: `development`,
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
+  mode: `development`,
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new EnvironmentPlugin({
+      API_KEY: process.env.WEATHER_API_KEY,
+      API_URL: process.env.WEATHER_API,
+    }),
+  ],
 });
